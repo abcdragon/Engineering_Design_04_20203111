6d46578404b32850cd75e4657147ef23cad33fc8
abcdragon <heybrostop@gmail.com>
2020-12-13 23:12:26 +0900

    PD control

--- pid_control/pid_control.ino
+++ pid_control/pid_control.ino
@@ -4,38 +4,45 @@
 #define PIN_SERVO 10
 #define PIN_IR A0
 
-#define _DIST_TARGET 255
-#define _DIST_MIN 100
-#define _DIST_MAX 410
+#define _DIST_TARGET 255.0
+#define _DIST_MIN 100.0
+#define _DIST_MAX 410.0
 
-#define _DIST_ALPHA 0.1
+#define _DIST_ALPHA 0.18
+
+#define LENGTH 30
+#define k_LENGTH 5
 
 #define _DUTY_MIN 1060 // 위
-#define _DUTY_NEU 1480 
-#define _DUTY_MAX 1900 // 아래
+#define _DUTY_NEU 1400 
+#define _DUTY_MAX 1820 // 아래
 
-#define _SERVO_ANGLE 1.0
-#define _SERVO_SPEED 3.5
+#define _SERVO_SPEED 1000 // servo speed limit (unit: degree/second)
+#define _RAMPUP_TIME 360 // servo speed rampup (0 to max) time (unit: ms)
 
 #define _INTERVAL_DIST 20 
 #define _INTERVAL_SERVO 20 
 #define _INTERVAL_SERIAL 100 
 
-#define _KP 3
-#define _KD 80
+#define START _DUTY_MIN + 100
+#define END _DUTY_MAX - 100
 
-#define A 70
-#define B 320
+#define _KP 3
+#define _KD 90
 
 Servo myservo;
 
+int iter;
+float dist_list[LENGTH], sum, dist_raw, dist_ema, alpha;
+
 float dist_target; // location to send the ball
-float dist_raw, dist_ema;
 
 unsigned long last_sampling_time_dist, last_sampling_time_servo, last_sampling_time_serial; 
 bool event_dist, event_servo, event_serial;
 
-int duty_chg_per_interval; 
+int duty_chg_max; // maximum speed, i.e., duty difference per interval (unit: us/interval)
+int duty_chg_per_interval; // current speed (unit: us/interval)
+int duty_chg_adjust; // duty accelration per interval during ramp up/down period (unit: us/interval^2)
 int duty_target, duty_curr;
 float error_curr, error_prev, control, pterm, dterm, iterm;
 
@@ -45,13 +52,21 @@ void setup() {
   myservo.attach(PIN_SERVO);
   pinMode(PIN_LED, OUTPUT);
 
-
+  iter = 0; sum = 0;
+  alpha = _DIST_ALPHA;
+  dist_raw = sum = dist_ema = 0.0;
   error_curr = error_prev = 0.0;
   
-  myservo.writeMicroseconds(_DUTY_NEU);
   duty_target = duty_curr = _DUTY_NEU;
+  myservo.writeMicroseconds(duty_curr);
+
+  iterm = dterm = pterm = 0.0;
   
-  duty_chg_per_interval = (_DUTY_MAX - _DUTY_MIN) * (_SERVO_SPEED / _SERVO_ANGLE) * (_INTERVAL_SERVO / 1000.0);
+  duty_chg_max = (float)(_DUTY_MAX - _DUTY_MIN) * _SERVO_SPEED / 180 * _INTERVAL_SERVO / 1000;
+  duty_chg_adjust = (float) duty_chg_max * _INTERVAL_SERVO / _RAMPUP_TIME;
+  duty_chg_per_interval = 0; // initial speed is set to 0.
+
+  last_sampling_time_dist = last_sampling_time_servo = last_sampling_time_serial = 0;
 }
 
 void loop() {
@@ -81,8 +96,9 @@ void loop() {
 
   if(event_dist) {
     event_dist = false;
-    dist_raw = ir_distance_filtered();
-
+    dist_raw = ir_distance_filter();
+    
+    
   // PID control logic
     error_curr = _DIST_TARGET - dist_raw; // 목표보다 가까우면 양, 멀면 음
     pterm = _KP * error_curr;
@@ -90,7 +106,7 @@ void loop() {
     control = pterm + dterm;
 
   // duty_target = f(duty_neutral, control)
-    duty_target = _DUTY_NEU + control * (control > 0 ? _DUTY_MAX - _DUTY_NEU : _DUTY_NEU - _DUTY_MIN) / 600.0;
+    duty_target = _DUTY_NEU + control;
 
   // keep duty_target value within the range of [_DUTY_MIN, _DUTY_MAX]
     duty_target = min(duty_target, _DUTY_MAX);
@@ -102,13 +118,28 @@ void loop() {
   if(event_servo){
     event_servo = false;
     // adjust duty_curr toward duty_target by duty_chg_per_interval
-    if(duty_curr > duty_target){
+    if(duty_target > duty_curr) {
+      if(duty_chg_per_interval < duty_chg_max) {
+        duty_chg_per_interval += duty_chg_adjust;
+        if(duty_chg_per_interval > duty_chg_max) duty_chg_per_interval = duty_chg_max;
+      }
       duty_curr += duty_chg_per_interval;
       if(duty_curr > duty_target) duty_curr = duty_target;
-    } else {
-      duty_curr -= duty_chg_per_interval;
+    }
+    else if(duty_target < duty_curr) {
+      if(duty_chg_per_interval > -duty_chg_max) {
+        duty_chg_per_interval -= duty_chg_adjust;
+        if(duty_chg_per_interval < -duty_chg_max) duty_chg_per_interval = -duty_chg_max;
+      }
+      duty_curr += duty_chg_per_interval;
       if(duty_curr < duty_target) duty_curr = duty_target;
     }
+    else {
+      duty_chg_per_interval = 0;
+    }
+
+    duty_curr = max(duty_curr, _DUTY_MIN);
+    duty_curr = min(duty_curr, _DUTY_MAX);
 
     // update servo position
     myservo.writeMicroseconds(duty_curr);
@@ -127,16 +158,63 @@ void loop() {
     Serial.print(",duty_curr:");
     Serial.print(map(duty_curr,1000,2000,410,510));
     Serial.println(",Min:100,Low:200,dist_target:255,High:310,Max:410");
+  }
+}
+
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+
+float real[] = {0.0, 70.0, 122.0, 175.0, 204.0, 225.0, 249.0, 280.0, 295.0};
+float ideal[] = {0.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 415.0};
+
+int search(float target){
+  int mid, low = 0, high = 8;
+  while(low < high){
+    mid = (low + high) / 2;
+    if(real[mid] >= target) high = mid;
+    else low = mid + 1;
+  }
+  return high;
+}
+
+float mapping(void){
+  dist_raw = ir_distance();
+  
+  int i = search(dist_raw);
+  return ideal[i - 1] + (ideal[i] - ideal[i - 1]) / (real[i] - real[i - 1]) * (dist_raw - real[i - 1]);  
+}
+
+float ir_distance_filter() {
+  sum = 0;
+  iter = 0;
+  while (iter < LENGTH)
+  {
+    dist_list[iter] = mapping();
+    sum += dist_list[iter];
+    iter++;
+  }
+  for (int i = 0; i < LENGTH-1; i++){
+    for (int j = i+1; j < LENGTH; j++){
+      if (dist_list[i] > dist_list[j]) {
+        float tmp = dist_list[i];
+        dist_list[i] = dist_list[j];
+        dist_list[j] = tmp;
+      }
     }
-}
-const float coE[] = {-0.0000162, 0.0102249, -0.6898655, 105.5885859};
+  }
+  
+  for (int i = 0; i < k_LENGTH; i++) {
+    sum -= dist_list[i];
+  }
+  for (int i = 1; i <= k_LENGTH; i++) {
+    sum -= dist_list[LENGTH-i];
+  }
 
-float ir_distance(){ // return value unit: mm
-  float value, volt = float(analogRead(PIN_IR));
-  value = ((6762.0 / (volt - 9.0)) - 4.0) * 10.0;
-  return coE[0] * pow(value, 3) + coE[1] * pow(value, 2) + coE[2] * value + coE[3];
-}
-
-float ir_distance_filtered(){ // return value unit: mm
-  return dist_ema = _DIST_ALPHA * ir_distance() + (1 - _DIST_ALPHA) * dist_ema;
+  float dist_cali = sum / (LENGTH - 2 * k_LENGTH);
+  dist_ema = alpha * dist_cali + (1 - alpha) * dist_ema;
+  return dist_ema;
 }
